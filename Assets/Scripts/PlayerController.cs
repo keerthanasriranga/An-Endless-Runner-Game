3ee6fc5d9f0377f9711db8c2a43f1de8a976845a
keerthanasriranga <keerthanasriranga11@gmail.com>
2020-09-02 10:59:40 -0700

    Collision detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -77,4 +77,13 @@ public class PlayerController : MonoBehaviour
     {
         direction.y = jumpForce;
     }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.transform.tag == "Obstacle")
+        {
+            PlayerManager.gameOver = true;
+        }
+    }
+
 }
